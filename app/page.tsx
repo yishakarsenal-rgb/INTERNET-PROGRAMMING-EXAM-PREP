df3cc508450688f664fcb1ccae0d018cef0efc60
yishakarsenal-rgb <yisahkarsenal@gmail.com>
2026-09-21 20:52:20 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
         <footer className="py-4 px-6 border-t border-border/50 text-center">
           <p className="text-xs text-muted-foreground">
             Made <span className="text-red-400"></span> by Yishak Mekuannent
-         
+          </p>
         </footer>
       </main>
     </div>
